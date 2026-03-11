90cd368caf66c82fa9b019db87a00cd016ea797a
Ameer <aborayaameer@gmail.com>
2026-03-11 10:45:51 +0200

    Style HTML elements

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Doormat Navigation</p>
+      <p>Contact</p>
+      <p>Social Media Links</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h2>Reserve a Table</h2>
+      <p>Book your table online and enjoy a great dining experience.</p>
+    </main>
+  );
+}
+
+export default Main;
